fce52fcd17e362dab2d6e342101316602c846a48
sravanValluru <sravantinkumar@gmail.com>
2022-03-19 20:41:27 +0530

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import SearchCity from "./components/SearchCity";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <>
+      <div className="text-3xl">Weather App</div>
+      <SearchCity />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/SearchCity/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SearchCity() {
+  return <div>SearchCity</div>;
+}
