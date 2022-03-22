94c6994f35127d5825697d4f25bdc9e94d4be1ef
sravanValluru <sravantinkumar@gmail.com>
2022-03-22 21:49:38 +0530

    Favorites feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchCity from "./components/SearchCity";
 import CurrentWeather from "./components/CurrentWeather";
 import ForecastWeather from "./components/ForecastWeather";
 import AirPollution from "./components/AirPollution";
+import Favorites from "./components/Favorites";
 
 const api = {
   key: "67750f6daace7049e45faba8b8e6bcb6",
@@ -16,6 +17,7 @@ function App() {
   const [city, setCity] = useState("");
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     console.log("App: useEffect");
@@ -24,7 +26,8 @@ function App() {
         .then((res) => res.json())
         .then((data) => {
           console.log("Geolaction: data for city", data);
-          if (data.cod === "404") {
+          if (data.length == 0) {
+            console.log("Geolaction: data for city length 0");
             setLatitude(0);
             setLongitude(0);
           } else {
@@ -38,14 +41,21 @@ function App() {
   }, [city]);
 
   return (
-    <div className="mx-auto px-4 py-16 flex flex-col place-items-center mx-12 md:mx-20">
+    <div className="relative mx-auto px-4 py-16 flex flex-col place-items-center mx-12 md:mx-20">
       <div className="text-5xl text-indigo-600" style={{ fontWeight: 900 }}>
         Weather App
       </div>
       <SearchCity setCity={setCity} />
+      <Favorites favorites={favorites} />
       <div className="flex w-full place-content-between">
         {city && (
-          <CurrentWeather city={city} base={api.base} apiKey={api.key} />
+          <CurrentWeather
+            favorites={favorites}
+            setFavorites={setFavorites}
+            city={city}
+            base={api.base}
+            apiKey={api.key}
+          />
         )}
         {latitude != 0 && longitude != 0 && (
           <AirPollution
---
+++ src/components/Favorites/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export default function Favorites({ favorites }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div className="absolute right-6 top-15 z-20">
+      <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+        <button
+          type="submit"
+          className="w-full text-white flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-slate-600 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-500"
+          onClick={handleClick}
+        >
+          Favorite Cities ({favorites.length})
+        </button>
+      </div>
+      <div className={` ${!isVisible ? "hidden" : ""}`}>
+        {favorites.length > 0 &&
+          favorites.map((city) => (
+            <div
+              key={city}
+              className="bg-white text-slate-900 mt-3 p-2 text-center shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0"
+            >
+              {city.charAt(0).toUpperCase() + city.slice(1)}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
